e1a31af52d2d54ba09e979473eefa5af86a94f17
Vaishnaviyarikala <sindhuyarikala@gmail.com>
2025-01-17 22:18:35 +0530

    initial commit

---
+++ src/MarksCalculator.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class MarksCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numberOfSubjects = scanner.nextInt();
+
+        double[] marks = new double[numberOfSubjects];
+        double totalMarks = 0;
+
+        // Input marks for each subject
+        for (int i = 0; i < numberOfSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + (i + 1) + " (out of 100): ");
+            marks[i] = scanner.nextDouble();
+            totalMarks += marks[i];
+        }
+
+        // Calculate average percentage
+        double averagePercentage = totalMarks / numberOfSubjects;
+
+        // Calculate grade
+        String grade = calculateGrade(averagePercentage);
+
+        // Display results
+        System.out.println("\n--- Results ---");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+
+    // Method to calculate grade based on average percentage
+    private static String calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return "A+";
+        } else if (averagePercentage >= 80) {
+            return "A";
+        } else if (averagePercentage >= 70) {
+            return "B";
+        } else if (averagePercentage >= 60) {
+            return "C";
+        } else if (averagePercentage >= 50) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
